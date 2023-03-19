5ebbf2bd10bb408547f93519b319e996764a738f
Sreeram Ogirala <sreeramogirala@gmail.com>
2023-03-19 14:46:27 -0400

    back end validation

--- routes/feed.js
+++ routes/feed.js
@@ -1,9 +1,12 @@
 const express=require('express')
 
+const {body} = require('express-validator')
+
 const router=express.Router()
 const FeedController=require('../controllers/feed')
 
 router.get('/posts',FeedController.getPosts)
-router.post('/post',FeedController.postPosts)
+router.post('/post',[body('title').trim().isLength({min:5}),
+                    body('content').trim().isLength({min:5})],FeedController.postPosts)
 
 module.exports=router
