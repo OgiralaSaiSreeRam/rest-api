f9d16d29b4743e04afa9636c40a67485f1d918f0
Sreeram Ogirala <sreeramogirala@gmail.com>
2023-03-19 14:18:04 -0400

    adding more json fields

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,7 +1,18 @@
 exports.getPosts=(req,res,next)=>{
 
-    console.log(req.body);
-    res.status(200).json({title:'potter',price:20.67})
+    // console.log(req.body);
+    res.status(200).json({ posts: [
+        {
+          _id: '1',
+          title: 'First Post',
+          content: 'This is the first post!',
+          imageUrl: 'images/goku.jpeg',
+          creator: {
+            name: 'goku'
+          },
+          createdAt: new Date()
+        }
+      ]})
 }
 
 exports.postPosts=(req,res,next)=>{
@@ -11,6 +22,10 @@ exports.postPosts=(req,res,next)=>{
     //create a post and store in db
     res.status(201).json({
         message:'post saved successfully!!',
-        post: { id: new Date().toISOString(), title: title, content: content }
+        post: { _id: new Date().toISOString(), title: title, content: content, imageUrl:'images/goku.jpg', creator:{
+            name:'sreeram',
+        } ,
+        createdAt: new Date().toISOString()
+    }
     })
 }
