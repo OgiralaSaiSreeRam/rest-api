c6271d9d95f88e4d199baa4ff88c236060322cb2
Sreeram Ogirala <sreeramogirala@gmail.com>
2023-03-20 00:07:09 -0400

    login and signup

---
+++ models/user.js
@@ -0,0 +1,27 @@
+const mongoose=require('mongoose')
+const Schema=mongoose.Schema
+
+const UserSchema= new Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    state:{
+        type:String,
+        default: 'I am new'
+    },
+    posts:[{
+        type:Schema.Types.ObjectId,
+        ref:'Post'
+    }]
+})
+
+module.exports= mongoose.model('User',UserSchema)
