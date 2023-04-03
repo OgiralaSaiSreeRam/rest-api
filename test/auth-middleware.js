732350f56e557f9ef22ee6d02598ffb71e69f587
Sreeram Ogirala <sreeramogirala@gmail.com>
2023-04-03 17:56:54 -0400

    checking for userId field

--- test/auth-middleware.js
+++ test/auth-middleware.js
@@ -1,4 +1,5 @@
 const expect = require('chai').expect;
+const jwt=require('jsonwebtoken')
 
 const authMiddleware = require('../middleware/is-auth');
 
@@ -23,4 +24,22 @@ describe('Auth middleware', function() {
     };
     expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();  // will throw an error because we cannot perform a split operation when the authHeader is only one string long
   });
+
+  it('should throw an error if user id is not assigned to req',function(){
+    
+    const req={
+      get: function(headerName){
+        return 'Bearer jsgvghgsdvfdhwgghwghfeg' //overriding the get function to return something
+      }
+    }
+
+    jwt.verify=function(){ //overriding the verify function so that it returns an objewct with userId field
+      // overriding globally meaning that any future referneces of this jwt.verify will return this 
+      return {userId: 'abc'}
+    }
+    authMiddleware(req,{},()=>{}) //manually calling the authmiddleware function
+    expect(req).to.have.property('userId')
+
+
+  })
 });
